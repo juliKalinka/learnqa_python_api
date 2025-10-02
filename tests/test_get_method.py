06e9fca09a340794d92ba8f98cb030991bc72257
Akasheva Yuliya <yuliya.akasheva@rt.ru>
2025-10-02 17:23:35 +0300

    Ex12: Тест запроса на метод header

--- tests/test_get_method.py
+++ tests/test_get_method.py
@@ -1,6 +1,5 @@
 import requests
 from lib.base_case import BaseCase
-from lib.assertions import Assertions
 
 
 class TestGetMethods(BaseCase):
@@ -9,4 +8,12 @@ class TestGetMethods(BaseCase):
         self.some_cookie = self.get_cookie(response1, "HomeWork")
         print(self.some_cookie)
         accepted_cookie = "hw_value"
-        assert self.some_cookie == accepted_cookie,f"Accepted cookie {accepted_cookie} is not equal {self.some_cookie}"
+        assert self.some_cookie == accepted_cookie, f"Accepted cookie {accepted_cookie} is not equal {self.some_cookie}"
+
+    def test_get_header(self):
+        response1 = requests.get("https://playground.learnqa.ru/api/homework_header")
+        print(response1.headers)
+        assert True
+        self.some_header = self.get_header(response1, "x-secret-homework-header")
+        accepted_header = "Some secret value"
+        assert self.some_header == accepted_header, f"Accepted cookie {accepted_header} is not equal {self.some_header}"

0930adaafd3c8e6f78ace174b7ca4f98f04249ef
Akasheva Yuliya <yuliya.akasheva@rt.ru>
2025-10-02 13:23:24 +0300

    Корректирую условия для phrase

--- tests/test_phrase.py
+++ tests/test_phrase.py
@@ -2,4 +2,4 @@ class TestPhrase:
 
     def test_check_len_15_phrase(self):
         phrase = input("Set a phrase less then 15 simbols: ")
-        assert len(phrase) <= 15, f"Input phrase more then 15 simbols or equal"
+        assert len(phrase) < 15, f"Input phrase '{phrase}' more then 15 simbols or equal"

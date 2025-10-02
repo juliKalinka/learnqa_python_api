0176fbbc1124421118adbb0791d38f4c80319f19
Akasheva Yuliya <yuliya.akasheva@rt.ru>
2025-10-02 13:28:02 +0300

    убираю тест не относящийся к классу

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -53,3 +53,6 @@ class TestUserAuth(BaseCase):
             0,
             f"User is authorized with condition {condition}"
         )
+    def test_check_len_15_phrase(self):
+        phrase = input("Set a phrase less then 15 simbols: ")
+        assert len(phrase) >= 15, f"Input phrase more then 15 simbols or equal"

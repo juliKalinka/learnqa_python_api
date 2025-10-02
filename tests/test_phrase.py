5448f554916b10d7cd3daa2ae37edea86500c1e7
Akasheva Yuliya <yuliya.akasheva@rt.ru>
2025-10-02 13:22:04 +0300

    Ex10: Тест на короткую фразу

---
+++ tests/test_phrase.py
@@ -0,0 +1,5 @@
+class TestPhrase:
+
+    def test_check_len_15_phrase(self):
+        phrase = input("Set a phrase less then 15 simbols: ")
+        assert len(phrase) <= 15, f"Input phrase more then 15 simbols or equal"
